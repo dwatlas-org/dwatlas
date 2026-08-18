e59b42e0d91a1d95bc4a13dbab06f6bea218417f
meucorre <contato@meucorre.com.br>
2026-08-18 10:02:22 -0300

    env: initial setup

---
+++ api/src/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"Hello": "World"}
+
+
+@app.get("/dashboard/{id}")
+def dashboard(id: int, name: str | None = None):
+    return {"id": id, "name": name}
+
+
+@app.post("/auth/login")
+def login(username: str, password: str):
+    pass
---
+++ ui/README.md
@@ -0,0 +1,1 @@
+Minimal [Vite setup](https://vite.dev/guide/) to get React working with [React Router](https://reactrouter.com/home) (data mode) and [shadcn/ui](https://ui.shadcn.com/docs).
