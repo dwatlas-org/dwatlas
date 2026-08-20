d48b40ad567405846e96b304d86f06c4536fc8f5
Quinn Aurich <quinnaurich@posteo.net>
2026-08-20 15:42:29 +0200

    move business logic from routes to service module

---
+++ server/api/user/service.py
@@ -0,0 +1,38 @@
+from sqlmodel import Session
+
+from .database import sqlite_engine
+from .models import User
+
+
+def get_users_all():
+    with Session(sqlite_engine) as session:
+        users = session.query(User).all()
+    return users
+
+
+def create_user(username: str, email: str | None, full_name: str | None, disabled: bool | None):
+    with Session(sqlite_engine) as session:
+        user = User(username=username, email=email, full_name=full_name, disabled=disabled)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+    return user
+
+
+def delete_user(user_id: int):
+    with Session(sqlite_engine) as session:
+        user = session.query(User).filter_by(id=user_id).first()
+        session.delete(user)
+        session.commit()
+
+
+def update_user(user_id: int, username: str | None, email: str | None, full_name: str | None, disabled: bool | None):
+    with Session(sqlite_engine) as session:
+        user = session.query(User).filter_by(id=user_id).first()
+        user.username = username
+        user.email = email
+        user.full_name = full_name
+        user.disabled = disabled
+        session.commit()
+        session.refresh(user)
+    return user
