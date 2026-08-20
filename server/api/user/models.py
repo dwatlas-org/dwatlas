3cdd12993b4641eeb405fa55c8c45dcf38cdaf30
Quinn Aurich <quinnaurich@posteo.net>
2026-08-20 16:58:55 +0200

    start using SQLModel properly

--- server/api/user/models.py
+++ server/api/user/models.py
@@ -2,9 +2,21 @@ from sqlmodel import Field, SQLModel
 from pydantic import EmailStr
 
 
-class User(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    username: str
+class UserBase(SQLModel):
+    username: str = Field(index=True)
     email: EmailStr | None = None
     full_name: str | None = None
     disabled: bool | None = None
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str = Field()
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserPublic(UserBase):
+    id: int
