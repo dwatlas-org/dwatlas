40c645c8e6c9ecd44eb41fe4c913b09d3336acd3
Quinn Aurich <quinnaurich@posteo.net>
2026-08-20 15:45:36 +0200

    get User objects efficiently...

--- server/api/user/service.py
+++ server/api/user/service.py
@@ -21,14 +21,14 @@ def create_user(username: str, email: str | None, full_name: str | None, disable
 
 def delete_user(user_id: int):
     with Session(sqlite_engine) as session:
-        user = session.query(User).filter_by(id=user_id).first()
+        user = session.query(User).get(user_id)
         session.delete(user)
         session.commit()
 
 
 def update_user(user_id: int, username: str | None, email: str | None, full_name: str | None, disabled: bool | None):
     with Session(sqlite_engine) as session:
-        user = session.query(User).filter_by(id=user_id).first()
+        user = session.query(User).get(user_id)
         user.username = username
         user.email = email
         user.full_name = full_name
