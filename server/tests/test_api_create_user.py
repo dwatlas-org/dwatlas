26a75960d0ee7b21cc50434fe6f07cb1ad5f10ab
Quinn Aurich <quinnaurich@posteo.net>
2026-08-25 17:28:41 +0200

    add conftest module for pytest fixtures fix test_api_create_user

--- server/tests/test_api_create_user.py
+++ server/tests/test_api_create_user.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 
+<<<<<<< HEAD
 
 def test_api_create_user(client: TestClient):
     response = client.post(
@@ -11,6 +12,23 @@ def test_api_create_user(client: TestClient):
             "password": "supersecret",
         },
     )
+||||||| parent of af4223c (add conftest module for pytest fixtures)
+=======
+from api.main import app
+
+
+def test_api_create_user(client: TestClient):
+    response = client.post(
+        "/user/",
+        json={
+            "username": "Yolanda",
+            "email": "yolanda@example.com",
+            "full_name": "Yolanda Thaire",
+            "password": "supersecret",
+        },
+    )
+    app.dependency_overrides.clear()
+>>>>>>> af4223c (add conftest module for pytest fixtures)
     data = response.json()
 
     assert response.status_code == 200
