0e0349f2829fb958046d13b815d234965964512e
Quinn Aurich <quinnaurich@posteo.net>
2026-08-25 17:31:06 +0200

    remove dependency clearing as that already happens in the fixture

---
+++ server/tests/test_api_create_user.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+
+
+def test_api_create_user(client: TestClient):
+    response = client.post(
+        "/user/",
+        json={
+            "username": "Yolanda",
+            "email": "yolanda@example.com",
+            "full_name": "Yolanda Thaire",
+            "password": "supersecret",
+        },
+    )
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data["username"] == "Yolanda"
+    assert data["email"] == "yolanda@example.com"
+    assert data["full_name"] == "Yolanda Thaire"
