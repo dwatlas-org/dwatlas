f46f77245a0d63d5b0ebc20a76d0699609c01393
Quinn Aurich <quinnaurich@posteo.net>
2026-08-27 12:48:44 +0200

    add some timestamp checks to tests for creating/updating user

--- server/tests/test_api_create_user.py
+++ server/tests/test_api_create_user.py
@@ -1,8 +1,12 @@
-from api.main import app
+import datetime
+
 from fastapi.testclient import TestClient
+
+from api.main import app
 
 
 def test_api_create_user(client: TestClient):
+    pre = datetime.datetime.now(datetime.UTC)
     response = client.post(
         "/user/",
         json={
@@ -14,8 +18,14 @@ def test_api_create_user(client: TestClient):
     )
     app.dependency_overrides.clear()
     data = response.json()
+    post = datetime.datetime.now(datetime.UTC)
 
     assert response.status_code == 200
     assert data["username"] == "Yolanda"
     assert data["email"] == "yolanda@example.com"
     assert data["full_name"] == "Yolanda Thaire"
+    datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
+    created = datetime.datetime.strptime(data["created"], datetime_format).replace(
+        tzinfo=datetime.UTC
+    )
+    assert pre < created < post
