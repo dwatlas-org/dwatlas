06a27d9d425413a8d4be36d19c2ba7df7fa327e1
daniel dias <contact@dwatlas.org>
2026-08-26 23:40:50 -0300

    feat(panels): postgres session manager & tests

--- server/api/main.py
+++ server/api/main.py
@@ -1,9 +1,32 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from postgres import create_async_engine
 
+from api.config import settings
 from api.user.routes import router as router_user
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    postgres_engine = create_async_engine(str(settings.POSTGRES_URL))
+    # sqlite_engine = create_async_engine(
+    #     settings.SQLITE_URL,
+    #     connect_args={"check_same_thread": False},
+    # )
+
+    yield
+
+    await postgres_engine.dispose()
+    # await sqlite_engine.dispose()
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    debug=settings.DEBUG,
+    lifespan=lifespan,
+)
 
 app.include_router(router_user)
 
@@ -25,6 +48,6 @@ def root():
     return {"Hello": "World"}
 
 
-@app.get("/dashboard/{id}")
-def dashboard(id: int, name: str | None = None):
-    return {"id": id, "name": name}
+@app.get("/panels/{id}")
+def panels(id: str, filters: dict | None = None):
+    return {"id": id, "filters": filters}
