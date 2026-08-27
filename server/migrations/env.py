d092161cd840fda62f01a7ce11f790a9a1c6d2c8
Quinn Aurich <quinnaurich@posteo.net>
2026-08-27 12:45:12 +0200

    rename alembic -> migrations add timesamp columns add 2 alembic versions (user table and adding timestamp columns)

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -1,8 +1,9 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
-
 from alembic import context
+from sqlmodel import SQLModel, create_engine
+
+from api.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,7 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -37,7 +38,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = settings.SQLITE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -56,11 +57,13 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    #    connectable = engine_from_config(
+    #        config.get_section(config.config_ini_section, {}),
+    #        prefix="sqlalchemy.",
+    #        poolclass=pool.NullPool,
+    #    )
+
+    connectable = create_engine(settings.SQLITE_URL)
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
