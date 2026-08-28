c0238e073a4b208b0ef988e5152f9fd9fa702d66
Quinn Aurich <quinnaurich@posteo.net>
2026-08-28 15:38:01 +0200

    add password hashing utility, use it when creating or updating users

--- server/tests/test_verify_password.py
+++ server/tests/test_verify_password.py
@@ -3,6 +3,13 @@ from api.user.service import hash_password, verify_password
 
 def test_verify_password():
     password = "supersecure"
-
     hashed_password = hash_password(password)
     assert verify_password(password, hashed_password)
+
+
+def test_verify_password_false():
+    password = "supersecure"
+    hashed_password = hash_password(password)
+    different_password = "notsecureatall"
+
+    assert not verify_password(different_password, hashed_password)
