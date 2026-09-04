72416e699cab19ad4e1851d796b5ae56b59c507c
Quinn Aurich <quinnaurich@posteo.net>
2026-09-04 16:36:50 +0200

    use fastapi / starlette constants for HTTP status codes

--- server/tests/test_api_delete_user.py
+++ server/tests/test_api_delete_user.py
@@ -1,3 +1,4 @@
+from fastapi import status
 from fastapi.testclient import TestClient
 
 
@@ -12,27 +13,27 @@ def test_api_delete_user(client: TestClient):
             "password": "supersecret",
         },
     )
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     # Make sure there's a user
     response = client.get("/user/all/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert len(data) == 1
 
     # Delete user
     response = client.delete("/user/1")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert data == {"ok": True}
 
     # Make sure there's no user
     response = client.get("/user/all/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert len(data) == 0
 
 
 def test_api_delete_user_notfound(client: TestClient):
     response = client.delete("/user/999")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_404_NOT_FOUND
