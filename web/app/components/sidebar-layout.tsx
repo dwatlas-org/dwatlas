bcb999628981f7dfa5297c689c65e007020161f4
daniel dias <contact@dwatlas.org>
2026-09-03 21:00:52 -0300

    nav: fix sidebar behavior on mobile

--- web/app/components/sidebar-layout.tsx
+++ web/app/components/sidebar-layout.tsx
@@ -13,8 +13,8 @@ export function SidebarLayout() {
       <PanelNav />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          {/*<SidebarTrigger className="-ml-1" />
-          <h3 className="text-xl font-semibold">Data Panels</h3>
+          <SidebarTrigger className="md:hidden -ml-1" />
+          {/*<h3 className="text-xl font-semibold">Data Panels</h3>
           <Separator orientation="vertical" className="mr-2 h-4" />*/}
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
