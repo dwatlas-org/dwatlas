ddaf99217ff298dc4d3ed40bae26eb9ef55b4b6c
Quinn Aurich <quinnaurich@posteo.net>
2026-09-07 16:45:09 +0200

    use new user factory in update user test

--- server/tests/test_api_update_user.py
+++ server/tests/test_api_update_user.py
@@ -7,33 +7,9 @@ from api.user.models import User
 from api.user.service import verify_password
 
 
-def test_api_update_user(client: TestClient):
-    # Create a user
-    username = "Yolanda"
-    email = "yolanda@example.com"
-    password = "supersecret"
-    full_name = "Yolanda Thaire"
-
-    response = client.post(
-        "/user/",
-        json={
-            "username": username,
-            "email": email,
-            "full_name": full_name,
-            "password": password,
-        },
-    )
-
-    assert response.status_code == 200
-    data = response.json()
-    assert data["id"]
-    assert data["username"] == username
-    assert data["email"] == email
-    assert data["full_name"] == full_name
+def test_api_update_user(client: TestClient, user: User):
     datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
-    updated_old = datetime.datetime.strptime(data["updated"], datetime_format).replace(
-        tzinfo=datetime.UTC
-    )
+    updated_old = user.updated.replace(tzinfo=datetime.UTC)
 
     # Update User
     new_username = "Aurora"
@@ -42,8 +18,8 @@ def test_api_update_user(client: TestClient):
     data = response.json()
     assert data["id"]
     assert data["username"] == new_username
-    assert data["email"] == email
-    assert data["full_name"] == full_name
+    assert data["email"] == user.email
+    assert data["full_name"] == user.full_name
     assert (
         datetime.datetime.strptime(data["updated"], datetime_format).replace(
             tzinfo=datetime.UTC
@@ -57,8 +33,8 @@ def test_api_update_user(client: TestClient):
     data = response.json()
     assert data["id"]
     assert data["username"] == new_username
-    assert data["email"] == email
-    assert data["full_name"] == full_name
+    assert data["email"] == user.email
+    assert data["full_name"] == user.full_name
     assert (
         datetime.datetime.strptime(data["updated"], datetime_format).replace(
             tzinfo=datetime.UTC
@@ -67,20 +43,13 @@ def test_api_update_user(client: TestClient):
     )
 
 
-def test_api_update_user_password_hash(client: TestClient, session: Session):
-    not_a_hash = "notahash"
-    user = User(username="malka", hashed_password=not_a_hash)
-    session.add(user)
-    session.commit()
-
-    user = session.get(User, 1)
-    assert user.hashed_password == not_a_hash
-
+def test_api_update_user_password_hash(
+    client: TestClient, session: Session, user: User
+):
     # Update User
     new_password = "supersecure123!"
     response = client.post("/user/update/1", json={"password": new_password})
     assert response.status_code == 200
 
     user = session.get(User, 1)
-    assert user.hashed_password != not_a_hash
     assert verify_password(new_password, user.hashed_password)
