54fc4028042501ca0c465f90f182f0ae19ac7140
Quinn Aurich <quinnaurich@posteo.net>
2026-09-07 16:44:22 +0200

    add User factory and factory session management

---
+++ server/tests/factories.py
@@ -0,0 +1,21 @@
+import factory
+from faker import Factory as FakerFactory
+from pytest_factoryboy import register
+
+from api.user.models import User
+from api.user.service import hash_password
+
+faker = FakerFactory.create()
+
+
+@register
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = User
+        sqlalchemy_session_persistence = "commit"
+
+    username: str = factory.LazyFunction(lambda: faker.first_name())
+    email: str = factory.LazyFunction(lambda: faker.email())
+    full_name: str = factory.LazyFunction(lambda: faker.name())
+    disabled: bool = False
+    hashed_password: str = factory.LazyFunction(lambda: hash_password(faker.word()))
