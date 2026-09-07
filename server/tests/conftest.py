86a512c7336d30789caa49c0778f42212410bdd2
Quinn Aurich <quinnaurich@posteo.net>
2026-09-07 16:44:22 +0200

    add User factory and factory session management

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,19 +1,26 @@
 import pytest
+from factory.alchemy import SQLAlchemyModelFactory
 from fastapi.testclient import TestClient
+from pytest_factoryboy import register
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
 
 from api.main import app
 from api.user.database import get_session
 
+from .factories import UserFactory
 
-@pytest.fixture(name="session")
+
+@pytest.fixture(name="session", scope="module")
 def session_fixture():
     engine = create_engine(
         "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
     )
     SQLModel.metadata.create_all(engine)
     with Session(engine) as session:
+        # This is a hack I found to make the factories use the right session object
+        for factory in SQLAlchemyModelFactory.__subclasses__():
+            factory._meta.sqlalchemy_session = session
         yield session
 
 
@@ -26,3 +33,6 @@ def client_fixture(session: Session):
     client = TestClient(app)
     yield client
     app.dependency_overrides.clear()
+
+
+register(UserFactory)
---
+++ server/tests/test_factories.py
@@ -0,0 +1,34 @@
+import pytest
+
+from api.user.models import User
+
+
+def test_user_factory(session, user):
+    assert user
+    assert user.username
+    assert user.hashed_password.startswith("$argon2id")
+    assert user.email
+    assert user.full_name
+    assert not user.disabled
+    assert user.created
+    assert user.updated
+    assert user.last_login is None
+
+    user_db = session.get(User, 1)
+    assert user_db
+    assert user_db.username == user.username
+    assert user_db.hashed_password == user.hashed_password
+    assert user_db.email == user.email
+    assert user_db.disabled == user.disabled
+    assert user_db.created == user.created
+    assert user_db.updated == user.updated
+    assert user_db.last_login == user.last_login
+
+
+@pytest.mark.parametrize("user__username", ["Dandara"])
+@pytest.mark.parametrize("user__email", ["dandara@dwatlas.org"])
+@pytest.mark.parametrize("user__disabled", [True])
+def test_user_factory_parametrized(session, user):
+    assert user.username == "Dandara"
+    assert user.email == "dandara@dwatlas.org"
+    assert user.disabled is True
