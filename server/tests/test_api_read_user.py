6db69891934c8cd090807abc3fe0c64ab953e141
Quinn Aurich <quinnaurich@posteo.net>
2026-09-07 17:15:00 +0200

    add second_user and third_user fixtures use user factory in api read user and read users tests

--- server/tests/test_api_read_user.py
+++ server/tests/test_api_read_user.py
@@ -1,33 +1,18 @@
 from fastapi.testclient import TestClient
 
+from api.user.models import User
 
-def test_api_read_user(client: TestClient):
-    # Create a user
-    username = "Yolanda"
-    email = "yolanda@example.com"
-    password = "supersecret"
-    full_name = "Yolanda Thaire"
 
-    response = client.post(
-        "/user/",
-        json={
-            "username": username,
-            "email": email,
-            "full_name": full_name,
-            "password": password,
-        },
-    )
-    assert response.status_code == 200
-
+def test_api_read_user(client: TestClient, user: User):
     # Read user
     response = client.get("/user/1")
     assert response.status_code == 200
     data = response.json()
 
     assert data["id"]
-    assert data["username"] == username
-    assert data["email"] == email
-    assert data["full_name"] == full_name
+    assert data["username"] == user.username
+    assert data["email"] == user.email
+    assert data["full_name"] == user.full_name
     assert "password" not in data
     assert "hashed_password" not in data
 
