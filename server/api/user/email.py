352ea44b2b731980e33781cc3c1b222b8862093b
Quinn Aurich <quinnaurich@posteo.net>
2026-09-17 16:38:43 +0200

    add smtp email logic and config values

--- server/api/user/email.py
+++ server/api/user/email.py
@@ -3,6 +3,8 @@ from email.message import EmailMessage
 from sys import stdout
 from typing import IO
 
+import aiosmtplib
+
 from api.config import settings
 
 
@@ -10,6 +12,10 @@ class EmailService:
     async def send(self, messages: Iterable[EmailMessage]) -> None:
         if settings.EMAIL_PROVIDER == "console":
             return await self.send_console(messages)
+        elif settings.EMAIL_PROVIDER == "smtp":
+            return await self.send_smtp(messages)
+        elif settings.EMAIL_PROVIDER == "mailersend":
+            return await self.send_mailersend(messages)
         else:
             raise NotImplementedError("Provider not implemented")
 
@@ -21,5 +27,17 @@ class EmailService:
             stream.write("-" * 79)
             stream.write("\n")
 
-    async def send_mailerlite(self, messages: Iterable[EmailMessage]) -> None:
+    async def send_mailersend(self, messages: Iterable[EmailMessage]) -> None:
         raise NotImplementedError
+
+    async def send_smtp(self, messages: Iterable[EmailMessage]) -> None:
+        for message in messages:
+            await aiosmtplib.send(
+                message,
+                hostname=settings.EMAIL_SMTP_HOST,
+                port=settings.EMAIL_SMTP_PORT,
+                username=settings.EMAIL_SMTP_USER,
+                password=settings.EMAIL_SMTP_PASSWORD,
+                start_tls=settings.EMAIL_SMTP_TLS,
+                timeout=10,
+            )
